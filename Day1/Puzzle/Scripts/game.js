f73377f9d824a7c9a73e3d47d4fd68a06c5e15e0
Cranky Crab <unocomputerscience+oddoyster@gmail.com>
2017-06-28 14:00:31 +0000

    Day 2 commit Cranky Crab

--- Day1/Puzzle/Scripts/game.js
+++ Day1/Puzzle/Scripts/game.js
@@ -1,31 +1,48 @@
 var passcode = ~~(Math.random() * 999);
 var tries = 10;
-console.log("Guess a number from 0 to 999");
 
-while (guess != passcode && tries > 0)
+var clueText = document.getElementById("clues");
+
+var button = document.getElementById("guess-button");
+var number = document.getElementById("guess-number");
+
+var triesText = document.getElementById("tries")
+
+button.addEventListener("click", guessNumber);
+triesText.innerHTML = "Number of tries left:" + tries;
+
+function guessNumber()
 {
     console.log("you have " + tries + " guesses left")
-    var guess = prompt("Enter a number(0 to 999)");
+    
+    var guess = number.value;
     tries = tries - 1;
-    if (guess > passcode) 
-    {
-        console.log("Too high");
-    }
-    if (guess < passcode)
-    {
-        console.log("Too low");
-    }
+    triesText.innerHTML = "Number of tries left:" + tries;
     if (guess == passcode)
     {
-        console.log("You Win!");
+        document.body.innerHTML = "<h1>You Win!</h1>";
     }
     else if (tries <= 0)
     {
-        console.log("You Loose.")
+        document.body.innerHTML = "<h1>You Lose!</h1>" +"<p>Thw correct answer was "+ passcode +"</p>"
+        
         console.log (passcode)
     }
     else
     {
         console.log ("Wrong. Try again");
+        giveClue(guess)
+    }
+}
+
+function giveClue(guess)
+{
+    if (guess < passcode)
+    {
+        clueText.innerHTML += "<li>" + guess +" is too low!" +"</li>"
+    }
+    if (guess > passcode) 
+    {
+        clueText.innerHTML += "<li>" + guess + " is too high" + "</li>";
     }
 }
